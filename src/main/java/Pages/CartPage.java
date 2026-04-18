5e0f4d6a4f2e544df68d44a3aac58652f809f7f6
vidyaprakasam <prakasamvidya@gmail.com>
2026-04-18 08:23:24 +0100

    cart page modified

--- src/main/java/Pages/CartPage.java
+++ src/main/java/Pages/CartPage.java
@@ -5,6 +5,8 @@ public class CartPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("cart page");
+		System.out.println("cart page modified");
+
 
 	}
 
