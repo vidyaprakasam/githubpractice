027b8c840b2cff5ed46277dfb9141ee7a737a610
vidya-11 <vidya.pecmca@gmail.com>
2026-04-19 19:43:47 +0100

    orderdevelopment

--- src/main/java/mainAssessment/SearchTest.java
+++ src/main/java/mainAssessment/SearchTest.java
@@ -4,6 +4,8 @@ public class SearchTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("logic added");
+
 		System.out.println("search test added");
 
 
