65c616eec18f3d437b547dc456f17c4b711c359d
vidya-11 <vidya.pecmca@gmail.com>
2026-04-19 21:44:43 +0100

    featuretwo

--- src/main/java/mainAssessment/FeatureNew.java
+++ src/main/java/mainAssessment/FeatureNew.java
@@ -5,6 +5,8 @@ public class FeatureNew {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Feature one");
+		System.out.println("Feature two");
+
 
 	}
 
