1106c8da5c69b568e8d7472b8f40de5e5cfd450e
vidya-11 <vidya.pecmca@gmail.com>
2026-04-19 21:41:00 +0100

    feature one

---
+++ src/main/java/mainAssessment/FeatureNew.java
@@ -0,0 +1,11 @@
+package mainAssessment;
+
+public class FeatureNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Feature one");
+
+	}
+
+}
