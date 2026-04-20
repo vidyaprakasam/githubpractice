b2eb3e73511153ad2da938330336a9d149517d3b
vidya-11 <vidya.pecmca@gmail.com>
2026-04-20 14:44:59 +0100

    cherry 1

---
+++ src/main/java/mainAssessment/CherrypickTest.java
@@ -0,0 +1,11 @@
+package mainAssessment;
+
+public class CherrypickTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cherry-pick 1");
+
+	}
+
+}
